cf672f2526279c3214b24e984f7e4109e6f136b0
ZarelaMC <zarelamanrique87@gmail.com>
2024-04-07 22:55:55 -0500

    ListadoProductos-Finalizado

---
+++ Listado_ProdClient/Controllers/ProductoController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+//PASO 1: Librerías ADO .NET
+using Microsoft.Data;
+using Microsoft.Data.SqlClient;
+using Listado_ProdClient.Models;
+
+namespace Listado_ProdClient.Controllers
+{
+    public class ProductoController : Controller
+    {
+        //PASO 2: Variable de sólo lectura del tipo IConfiguration
+        public readonly IConfiguration  Configuration;
+
+        //PASO 3: Inyección de dependencias en el constructor para que el controller pueda acceder al archivo de configuración
+        public ProductoController(IConfiguration iConfig)
+        {
+            Configuration = iConfig;
+        }
+
+        //PASO 4: Función que retorne una lista de los productos
+        public List<Producto> listaProducto()
+        {
+            //PASO 4.1: Lista que almacenará las facturas
+            List<Producto> lProductos = new List<Producto>();
+
+            //PASO 4.2: Definir la cadena de conexión
+            SqlConnection cn = new SqlConnection(Configuration["ConnectionStrings:cn"]);
+
+            //PASO 4.3: Abrir la conexión
+            cn.Open();
+
+            //PASO 4.4: Llamar a la sentencia SQL y especificar la conexión a BD a usar
+            SqlCommand cmd = new SqlCommand("SP_LISTAR_PRODUCTOS", cn);
+
+            //PASO 4.5: Especificar el tipo de sentencia SQL que se ejecutará (BUENA PRÁCTICA)  
+            cmd.CommandType = System.Data.CommandType.Text;
+
+            //PASO 4.6: Almacenar en un objeto de lectura la información que traiga la sentencia SQL.  - Aquí se inicia la ejecución del comando SQL
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            //PASO 4.7: Leer la información traída con el método Read() el cuál lee los registros y se apoya del bucle While para avanzar al siguiente registro hasta llegar al último existente
+            while (dr.Read())
+            {
+                //PASO 4.8: Añadir los objetos Productos(cada línea de registros traídos por el "dr") a la lista declarada, esto usando el método Add() de la lista
+                lProductos.Add(new Producto()
+                {
+                    //PASO 4.9:Relacionar los atributos de la clase Producto con los datos de cada registro traído por el DataReader (representado por dr)
+                    ide_pro = int.Parse(dr[0].ToString()),
+                    des_pro = dr[1].ToString(),
+                    pre_pro = Double.Parse(dr[2].ToString()),
+                    sac_pro = int.Parse(dr[3].ToString()),
+                    smi_pro = int.Parse(dr[4].ToString()),
+                    uni_pro = dr[5].ToString()
+                });
+            }
+
+            //PASO 5: Cerrar la conexión
+            cn.Close();
+
+            //PASO 6: Devolver la lista llena
+            return lProductos;
+        }
+
+        public IActionResult listadoTablaProdutos()
+        {
+            return View(listaProducto());
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Listado_ProdClient/Models/Producto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace Listado_ProdClient.Models
+{
+    public class Producto
+    {
+        [DisplayName("CÓDIGO")]
+        public int ide_pro { get; set; }
+
+        [DisplayName("DESCRIPCIÓN")]
+        public string des_pro { get; set; }
+
+        [DisplayName("PRECIO")]
+        public Double pre_pro { get; set; }
+
+        [DisplayName("STOCK ACTUAL")]
+        public int sac_pro { get; set; }
+
+        [DisplayName("STOCK MÍNIMO")]
+        public int smi_pro { get; set; }
+
+        [DisplayName("TIPO UNIDAD")]
+        public string uni_pro { get; set; }
+    }
+}
